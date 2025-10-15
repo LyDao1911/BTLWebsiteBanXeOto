34294d37a751b0c5a00ab87d46a0a312da10a97c
LyDao1911 <lydao19112004@gmail.com>
2025-10-15 16:59:38 +0700

    BTLBanOto

---
+++ src/java/dao/Connect.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+import java.sql.*;
+/**
+ *
+ * @author Hong Ly
+ */
+public class Connect {
+    public static Connection getCon(){
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/sellingcar","root","");
+            return con;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}
